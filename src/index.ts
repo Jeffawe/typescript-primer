06195cc1655f6e69c815c802b76d1c0b0835fb9d
jeffawe <jeffery.ozoekwe-awagu@pau.edu.ng>
2021-02-04 14:49:31 +0100

    Student Registration Addition

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,40 @@ const counting: number = 10;
 
 console.log(firstname);
 
+export class Person {
+    //declare property types
+    firstName: string;
+    lastName: string;
+    gender: string;
+    height: number;
+    hobbies: string[];
+    birthday: Date;
+    isAlive: boolean;
+
+    constructor(firstName: string, lastName: string, gender: string, height: number = -1,
+        hobbies: string[] = [], birthday: Date, isAlive: boolean = true) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.height = height;
+        this.hobbies = hobbies;
+        this.birthday = birthday;
+        this.isAlive = isAlive;
+    }
+}; 
+
+let firstName = "Pius";
+let lastName = "Onobhayedo";
+let gender = "male";
+let height = 1.7;
+let hobbies = ['Tennis','Programming'];
+let birthday = new Date(1900,4,5);
+let isAlive = true;
+let person1 = new Person(firstName, lastName, gender, height, hobbies, birthday, isAlive)
+
+
+console.log(`Person 1 is ${person1.firstName} whose height is ${person1.height}and birthday is ${person1.birthday.toLocaleDateString()} and hobbies are ${person1.hobbies}`);
+
+
+
 
